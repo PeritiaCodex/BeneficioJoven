ff1604d4fb76272221b5ab6600a3cfa01e689572
Daniel Díaz Romero <149412157+A01801486@users.noreply.github.com>
2025-10-20 21:33:13 -0600

    Actualización de color & theme
    Inserto la implementación de pequeños cambios en los archivos de color y theme

--- app/src/main/java/mx/itesm/beneficiojoven/view/ui/theme/Color.kt
+++ app/src/main/java/mx/itesm/beneficiojoven/view/ui/theme/Color.kt
@@ -2,15 +2,43 @@ package mx.itesm.beneficiojoven.view.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+/**
+ * Archivo que centraliza la paleta de colores de la aplicación.
+ *
+ * Define los colores utilizados en el tema de Material Design y los colores
+ * personalizados para componentes específicos, como el fondo degradado.
+ */
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// --- Colores para el tema de la App ---
+val primary     = Color(0xFF0662FA)
+val onPrimary = Color(0xFF4663E8)
 
-val AppBarYellow       = Color(0xFFFFE082)
-val AppBarText         = Color(0xFF5D4037)
-val AppBackgroundCream = Color(0xFFFFF3D6)
-val SoftDivider        = Color(0xFFE6DDBF)
+val secondary    = Color(0xFF9A2CEC)
+val onSecondary    = Color(0xFF9756CB)
+val surface      = Color(0xFFFA52B4)
+val surfaceTint = Color(0xFFB9F6F8) // íconos
+val onSurface     = Color(0xFFCB72A0)
+val onSurfaceVariant = Color(0xFF4E4262)
+
+val tertiary       = Color(0xFFFF688B)
+val onTertiary     = Color(color = 0xFFFD727D)
+
+val error = Color(0xFFFF0474)
+
+
+val primaryContainer = Color(0xFFC2E2EB)
+
+
+
+val seeMe     = Color(color = 0xFFFF00E5)
+
+
+val outlineVariant  = Color(0xFF7A68FD)
+
+// --- Colores para el degradado del fondo ---
+val GradientStart      = Color(0xFF84FCEE)
+val GradientEnd        = Color(0xFF925DFF)
+
+// --- Colores adicionales ---
+val BrownSoft = Color(0xFF8D6E63)
+val GreenAccent = Color(0xFF1B5E20)
